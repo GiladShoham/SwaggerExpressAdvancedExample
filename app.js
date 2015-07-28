b05e64103806fb84478187302d42010df7ad254f
Gilad Shoham <gilad.shoham@sisense.com>
2015-07-28 15:20:22 +0300

    getting swagger with node.js

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 'use strict';
 
 var SwaggerExpress = require('swagger-express-mw');
-var app = require('express')();
+var express = require('express');
+var app = express();
 module.exports = app; // for testing
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(express.static(__dirname + '/api/swagger'));
+
+/*app.get('/swagger', function (req, res) {
+  res.send('Hello World!');
+});*/
 
 var config = {
   appRoot: __dirname // required config
