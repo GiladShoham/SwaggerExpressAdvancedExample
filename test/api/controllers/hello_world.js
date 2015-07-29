0c1808cb4b185dc6e8b0ef0a0ed1a91b48a08cd7
Gilad Shoham <gilad.shoham@sisense.com>
2015-07-29 18:49:01 +0300

    add testing for user controller

--- test/api/controllers/hello_world.js
+++ test/api/controllers/hello_world.js
@@ -47,4 +47,45 @@ describe('controllers', function() {
 
   });
 
+  describe('user_controller', function() {
+
+    describe('GET /hello', function() {
+
+      it('should return a default string', function(done) {
+
+        request(server)
+          .get('/hello2')
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function(err, res) {
+            should.not.exist(err);
+
+            res.body.should.eql('My name is, username!');
+
+            done();
+          });
+      });
+
+      it('should accept a name parameter', function(done) {
+
+        request(server)
+          .get('/hello2')
+          .query({ name: 'Gilad'})
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function(err, res) {
+            should.not.exist(err);
+
+            res.body.should.eql('My name is, Scott!');
+
+            done();
+          });
+      });
+
+    });
+
+  });
+
 });
