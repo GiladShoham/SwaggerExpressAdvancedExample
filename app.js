03bc629ee2103695b0edca54e764706fb49a1755
Gilad Shoham <gilad.shoham@sisense.com>
2015-07-29 18:48:28 +0300

    move parsedSwagger to swagger mw

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var path = require('path');
 var _ = require('underscore');
 var app = express();
 var resolve = require('./src/common/resolve');
+var yaml = require('js-yaml');
 
 module.exports = app; // for testing
 
@@ -23,37 +24,39 @@ app.use(express.static(__dirname + '/api/swagger'));
 var parsedSwagger;
 resolve.resolveYaml()
 .then(function (result){
-	parsedSwagger = result;
+	parsedSwagger = yaml.safeLoad(result);
+  app.get('/parsedSwagger', function (req, res) {
+    res.send(parsedSwagger);
+  });
+
+  var controllersDirs = [
+  "api/controllers",
+  "src/features"
+  ]
+
+  controllersDirs = _.map(controllersDirs, resolveAppPath);
+
+  var config = {
+      appRoot: __dirname, // required config
+      controllersDirs : controllersDirs,
+      swaggerFile : parsedSwagger
+    };
+
+  SwaggerExpress.create(config, function(err, swaggerExpress) {
+    if (err) { throw err; }
+
+    // install middleware
+    swaggerExpress.register(app);
+
+    var port = process.env.PORT || 10010;
+    app.listen(port);
+
+    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+  });
 });
 
-app.get('/parsedSwagger', function (req, res) {
-  res.send(parsedSwagger);
-});
 
-var controllersDirs = [
-	"api/controllers",
-	"src/features"
-]
-
-controllersDirs = _.map(controllersDirs, resolveAppPath);
-
-var config = {
-  appRoot: __dirname, // required config
-  controllersDirs : controllersDirs
-};
-
-SwaggerExpress.create(config, function(err, swaggerExpress) {
-  if (err) { throw err; }
-
-  // install middleware
-  swaggerExpress.register(app);
-
-  var port = process.env.PORT || 10010;
-  app.listen(port);
-
-  console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-});
 
 function resolveAppPath(to) {
     return path.resolve(__dirname, to);
-  };
+};
